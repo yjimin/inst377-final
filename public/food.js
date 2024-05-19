c7a8cda972095e52b405abf8b6e6f0b5770558a1
yjimin <jyoon110@umd.edu>
2024-05-19 14:10:05 -0400

    added options for food

--- public/food.js
+++ public/food.js
@@ -1,3 +1,28 @@
-appId = "688df902"
-appKey = "3cd8355a3914c25b04151a8fc99cf2a1"
+const appId = "1602f865";
+const appKey = "bf12eff41ed89d11ec705e2725cf9e5d";
 
+async function getRecipes() {
+    
+}
+
+async function loadPlanTable() {
+    var host = window.location.origin;
+    await fetch (`${host}/plans`)
+        .then(res => res.json())
+        .then(data => {
+            var table = document.createElement('table');
+            table.setAttribute('id','lists');
+
+            var tableRow = document.createElement('tr');
+            var tableHeading
+        });
+
+}
+function parsetableJSON() {
+    var recipes = document.getElementById('recipes').value;
+    var recipeList = recipes.split(',');
+    var recipeList = recipeList.map(recipe => recipe.trim());
+    return recipeList;
+}
+
+window.onload = loadPlanTable
